4a170fdf9af7da19f2465ff17c107e3c6ea9d6ee
Batrach <63921506+Batrach@users.noreply.github.com>
2022-11-14 20:54:28 +0300

    Update js.js

--- js.js
+++ js.js
@@ -118,19 +118,3 @@ function slide(){
     img1.classList.add('slide')
 }
 
-$(document).ready(function() {
-    // Обрабатывает клик на картинке
-    $('#img1-').click(function() {
-      // Получаем адрес картинки
-      var imgAddr = $(this).attr(&quot;src&quot;);
-      // Задаем свойство SRC картинке, которая в скрытом диве.
-      $('#img_big_block img').attr({src: imgAddr});
-      // Показываем скрытый контейнер
-      $('#img_big_block').fadeIn('slow');
-    });
-  
-    // Обрабатывает клик по большой картинке
-    $('#img_big_block').click(function() {
-      $(this).fadeOut();
-    });
-  });
